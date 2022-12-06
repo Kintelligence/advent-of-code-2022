1206ecae8cd0c5bfa5d6b731acdb2da8eefc67e1
Kenny Quach <kennyquach1@gmail.com>
2022-12-06 09:48:03 +0100

    feat: add solution for day 6

---
+++ day-6/src/main.rs
@@ -0,0 +1,40 @@
+extern crate shared;
+use std::collections::HashSet;
+
+use shared::io::Reader;
+
+fn main() {
+    let result = part_1(&mut Reader::open("input.txt").expect("expected reader"));
+
+    println!("{result}");
+
+    let result = part_2(&mut Reader::open("input.txt").expect("expected reader"));
+
+    println!("{result}");
+}
+
+fn part_1(reader: &mut Reader) -> u32 {
+    let mut buffer = String::new();
+    let input = reader.read_line(&mut buffer);
+
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(4)
+        .position(|slice| slice.iter().collect::<HashSet<&char>>().len() == 4)
+        .expect("expect match") as u32
+        + 4
+}
+
+fn part_2(reader: &mut Reader) -> u32 {
+    let mut buffer = String::new();
+    let input = reader.read_line(&mut buffer);
+
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(14)
+        .position(|slice| slice.iter().collect::<HashSet<&char>>().len() == 14)
+        .expect("expect match") as u32
+        + 14
+}
